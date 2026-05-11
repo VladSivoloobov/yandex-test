78017cadac6a62c549e5ee79f5e8ec52976efdf9
vladislav <sivolobovv3@gmail.com>
2026-05-11 03:53:55 +0300

    added lazy loads

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,25 +1,51 @@
-import { createHeader } from './header.js';
-import { createScrollingText } from './scrolling-text.js';
-import { Slider } from './slider.js';
-
 document.addEventListener('DOMContentLoaded', main);
 
 function main() {
-  const scrollingTextElements = document.querySelectorAll('.scrolling-text');
+  lazyLoad('.scrolling-text', async () => {
+    const { createScrollingText } = await import('./scrolling-text.js');
 
-  scrollingTextElements.forEach((item) => {
-    createScrollingText(item);
+    const scrollingTextElements = document.querySelectorAll('.scrolling-text');
+
+    scrollingTextElements.forEach((item) => {
+      createScrollingText(item);
+    });
   });
 
-  const header = document.querySelector('.header');
-  createHeader(header);
-  new Slider(document.querySelector('.members-section__members-slider'), {
-    navigation: document.querySelector('.members-section__navigation'),
-    gap: 20,
-    slidePerView: 3,
-    pagination: document.querySelector(
-      '.members-section__navigation .slider-pagination',
-    ),
-    draggable: false,
+  window.addEventListener(
+    'scroll',
+    async () => {
+      const header = document.querySelector('.header');
+      const { createHeader } = await import('./header.js');
+
+      createHeader(header);
+    },
+    { once: true },
+  );
+
+  lazyLoad('.members-section__members-slider', async () => {
+    const { Slider } = await import('./slider.js');
+
+    new Slider(document.querySelector('.members-section__members-slider'), {
+      navigation: document.querySelector('.members-section__navigation'),
+      gap: 20,
+      slidePerView: 3,
+      pagination: document.querySelector(
+        '.members-section__navigation .slider-pagination',
+      ),
+      draggable: false,
+    });
   });
+}
+
+function lazyLoad(selector, callback) {
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        callback();
+        observer.disconnect();
+      }
+    });
+  });
+
+  document.querySelectorAll(selector).forEach((item) => observer.observe(item));
 }
